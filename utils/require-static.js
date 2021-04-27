84e288251f47198a56131419cbf2d52a838849f8
Stanley Cassan <stanley@reezocar.com>
2021-04-27 16:03:34 +0200

    Create require-static.js

---
+++ utils/require-static.js
@@ -0,0 +1,21 @@
+/* eslint-disable import/no-dynamic-require, security/detect-non-literal-require */
+
+import { __DEV__ } from '../constants/env';
+
+export default (file, trycatch = false) => {
+   file = file.replace(/!/, '');
+
+   let r = null;
+   if (trycatch) {
+      try {
+         r = require(`../statics/${file}`);
+      } catch (error) {
+         if (__DEV__ && error.code !== 'MODULE_NOT_FOUND')
+            console.error(`MODULE_NOT_FOUND : ${file}`);
+      }
+   } else {
+      r = require(`../statics/${file}`);
+   }
+
+   return (r || {}).default;
+};
