ac8d4c423d7f68ccb1ff5905e30362c1f4c9d43b
Stanley Cassan <stanley@reezocar.com>
2021-04-27 16:03:16 +0200

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,32 @@
+module.exports = {
+   env: {
+      CONF_API_BASE: process.env.CONF_API_BASE,
+      CONF_API_STATIC_BASE: process.env.CONF_API_STATIC_BASE,
+      CONF_ENV: process.env.CONF_ENV,
+      N_ENV: process.env.NODE_ENV,
+   },
+   trailingSlash: true,
+
+   webpack: (config) => {
+      config.node = {
+         fs: 'empty',
+      };
+
+      config.module.rules.push({
+         test: /\.(css|gif|jpg|jpeg|png|svg)$/,
+         use: [
+            {
+               loader: 'file-loader',
+               options: {
+                  context: '',
+                  outputPath: 'static',
+                  publicPath: '/_next/static',
+                  name: '[path][name].[ext]?[contenthash]',
+               },
+            },
+         ],
+      });
+
+      return config;
+   },
+};
